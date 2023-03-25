e4be45bed27e33e31f888e4512284deaa71b9444
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-25 08:22:08 -0300

    Added Get-Disk.sh

---
+++ Get-Disk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+bay=$1
+ctrl_index="0"
+device_base="/sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/host0/port-0:0/expander-0:0/"
+bay=$((10#$bay))
+if [ -z "$1" ];then
+  echo "Usage: Get-Disk.sh XX"
+  echo "where XX corresponds to the bay you want to query."
+  echo "The returned value is the corresponding device in the format /dev/sdX"
+  exit 1
+fi
+
+allbays=$(find $device_base/port* -type f -name bay_identifier)
+disk=""
+for i in $allbays; do
+  curbay=$(cat $i)
+  if [ $curbay -eq $bay ]
+    disk=$(echo $i | rev | cut -d "/" -f 2 | cut -d "-" -f 1 | rev)":0"
+    disk=$(lsscsi | grep $disk | rev | awk '{print $1}' | rev)
+  fi
+done
+
+if [ -z "$disk" ];then
+  echo $disk
+else
+  echo "No disk found on bay $1 of the enclosure"
+fi
