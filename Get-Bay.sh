e02df43f72a7be364e59fc252fcb3333f7cf4947
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-25 07:24:17 -0300

    Added usage message

--- Get-Bay.sh
+++ Get-Bay.sh
@@ -2,6 +2,12 @@
 ctrl_index="0"
 device_base="/sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/host0/port-0:0/expander-0:0/"
 ctl=$(lsblk -n -o HCTL $1 | cut -d ":" -f 1-3)
+if [ -z "$1" ];then
+  echo "Usage: Get-Bay.sh /dev/sdX"
+  echo "the returned number corresponds to the baywhere disk sdX dis placed"
+  exit 1
+fi
+
 file="/sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/host0/port-0:0/expander-0:0/port-$ctl/end_device-$ctl/sas_device/end_device-$ctl/bay_identifier"
 if test -f "$file"; then
   bay=$(cat $file)
