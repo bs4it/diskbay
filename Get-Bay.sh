329596bc08eb631d7eaf774642ec194890a063d4
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-25 07:15:50 -0300

    Initial

---
+++ Get-Bay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ctrl_index="0"
+device_base="/sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/host0/port-0:0/expander-0:0/"
+ctl=$(lsblk -n -o HCTL $1 | cut -d ":" -f 1-3)
+file="/sys/devices/pci0000:00/0000:00:02.2/0000:02:00.0/host0/port-0:0/expander-0:0/port-$ctl/end_device-$ctl/sas_device/end_device-$ctl/bay_identifier"
+if test -f "$file"; then
+  bay=$(cat $file)
+  bay=$(printf "%02d" $bay)
+  echo $bay
+else
+  echo "$1 not found"
+fi
