d8bddbd85d41fa363963c3d8a8eac61db59e3836
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2023-03-25 09:14:16 -0300

    new file:   Format-Disk.sh

---
+++ Format-Disk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+bay=$1
+if [ -z "$1" ];then
+  echo ""
+  echo "Usage: Format-Disk.sh XX"
+  echo ""
+  echo "Where XX corresponds to the bay holding the disk you want to format."
+  exit 1
+fi
+bay=$((10#$bay))
+
+echo ""
+echo "If you proceed the disk on the bay $bay will be unmounted and formated. All data on this disk will be PERMANENTELY LOST."
+echo ""
+
+while ! [[ $accept = 'Y' || $accept = 'y' || $accept = 'N' || $accept = 'n' ]]
+do
+	echo -n -e "Do you really want to go ahead? (Y/N):"
+	read accept
+	case $accept in
+		y|Y)
+			echo ""
+			;;
+		n|N)
+			echo "Quitting, bye!"
+			sleep 1
+			exit 0
+			;;
+  	esac
+done
+
+disk=$($(dirname "$0")/Get-Disk.sh $bay)
